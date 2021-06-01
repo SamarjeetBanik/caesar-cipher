9943296df2acc7490880e83c0a3b5efc317874f8
SamarjeetBanik <samarjeetbanik@gmail.com>
2021-06-02 01:23:19 +0530

    initial commit

---
+++ cipherFunc.js
@@ -0,0 +1,33 @@
+function encrypt() {
+    var pt = document.getElementById("plainText").value
+    var skey = parseInt(document.getElementById("key").value)
+    var output = ''
+    
+    for(let i = 0; i < pt.length; i++) 
+    {
+        if(pt[i] === pt[i].toUpperCase()) {
+            output += String.fromCharCode((pt.charCodeAt(i) + skey - 65) % 26 + 65);
+        } 
+        else {
+            output += String.fromCharCode((pt.charCodeAt(i) + skey - 97) % 26 + 97);
+        }
+    }
+    document.getElementById("cipherText").innerHTML = output;
+}
+
+function decrypt() {
+    var ct = document.getElementById("textCipher").value
+    var skey = parseInt(document.getElementById("dkey").value)
+    var output = ''
+    
+    for(let i = 0; i < ct.length; i++) 
+    {
+        if(ct[i] === ct[i].toUpperCase()) {
+            output += String.fromCharCode((ct.charCodeAt(i) - skey - 65) % 26 + 65);
+        } 
+        else {
+            output += String.fromCharCode((ct.charCodeAt(i) - skey - 97) % 26 + 97);
+        }
+    }
+    document.getElementById("textPlain").innerHTML = output;
+}
